1e1de14cdc1415560408aee1c05442efd79929a9
zhuang99 <zilie_huang@ucsb.edu>
2019-12-14 14:51:20 -0800

    fixed function.h

--- Compound_trading.cpp
+++ Compound_trading.cpp
@@ -3,7 +3,6 @@
 #include"function.h"
 using namespace std;
 
-
 int main()
 {
     output();   
--- function.h
+++ function.h
@@ -4,5 +4,5 @@
 
 long long calculate_compound(double percentPerDay, long long initialFund, int days);
 void output();
-
+double pct_pl(long long initialFund, long long result);
 #endif
